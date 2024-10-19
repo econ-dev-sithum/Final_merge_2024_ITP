2517c07bc59740043d974e5f17294d2ac46a2138
econ-dev-sithum <it21239298@my.sliit.lk>
2024-10-20 01:44:57 +0530

    done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,19 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { store } from "../src/component/Transport/store/store";
+// import { store } from "../src/component/Transport/store/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { storeIn, persistor } from "../src/inventry/redux/store";
+import { store, persistor } from "../src/inventry/redux/store";
 import StoreContextProvider from "../src/food/Context/StoreContext";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // cded
   <BrowserRouter>
     <StoreContextProvider>
       <PersistGate persistor={persistor}>
-        <Provider store={(store, storeIn)}>
+        <Provider store={store}>
           <React.StrictMode>
             <App />
           </React.StrictMode>
