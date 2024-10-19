05f92d76678da6a9e400b19165fd1e2e7c69e35c
econ-dev-sithum <it21239298@my.sliit.lk>
2024-10-20 01:28:30 +0530

    axax

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // cded
   <BrowserRouter>
     <StoreContextProvider>
       <PersistGate persistor={persistor}>
