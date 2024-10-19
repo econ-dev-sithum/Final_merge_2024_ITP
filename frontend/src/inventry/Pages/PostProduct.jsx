1cc13de4262e6153fd97bb15ae9d43755f9ef137
AnjanaTinush <146495563+AnjanaTinush@users.noreply.github.com>
2024-10-20 01:15:59 +0530

    chsnge

--- frontend/src/inventry/Pages/PostProduct.jsx
+++ frontend/src/inventry/Pages/PostProduct.jsx
@@ -64,7 +64,7 @@ export default function PostProduct() {
     setNotification({ visible: true, message });
     setTimeout(() => {
       setNotification({ visible: false, message: '' });
-    }, 3000);
+    }, 5000);
   };
 
   if (loading) {
--- frontend/src/inventry/redux/store.js
+++ frontend/src/inventry/redux/store.js
@@ -1,33 +1,47 @@
-
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+
+// Importing reducers from both stores
 import userReducer from './user/userSlice';
 import productsReducer from './product/productSlice';
-import { productsApi } from './product/productApi';
+ import { productsApi } from './product/productApi';
 import cartReducer, { getCartTotal } from './cart/cartSlice';
 
+import transportSlice from '../../component/Transport/store/reducer';
+import { apiSlice } from '../../component/Transport/store/apiSlice';
+
+// Combine reducers from both stores
 const rootReducer = combineReducers({
   user: userReducer,
   products: productsReducer,
   cart: cartReducer,
-  [productsApi.reducerPath]: productsApi.reducer,
+  [productsApi.reducerPath]: productsApi.reducer,  // API reducer from storeIn
+  transport: transportSlice,  // Transport reducer from store
+  [apiSlice.reducerPath]: apiSlice.reducer,       // API reducer from store
 });
 
+// Persistence configuration
 const persistConfig = {
   key: 'root',
   version: 1,
   storage,
 };
 
+// Persisted reducer
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const storeIn = configureStore({
+// Configure store with combined reducers and middleware
+export const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }).concat(productsApi.middleware),
+    getDefaultMiddleware({ serializableCheck: false })  // Disable serializable check for redux-persist
+      .concat(productsApi.middleware)  // Add products API middleware
+      .concat(apiSlice.middleware),    // Add transport API middleware
 });
 
-storeIn.dispatch(getCartTotal());
+// Dispatch initial actions (e.g., getCartTotal)
+store.dispatch(getCartTotal());  // Dispatch initial cart calculation
 
-export const persistor = persistStore(storeIn);
+// Persist store
+export const persistor = persistStore(store);
