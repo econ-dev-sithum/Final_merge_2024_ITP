4fa23e27760219bcfc4d645bf9265b297b9f72ef
Sathsara Sithum <99238978+IT21239298@users.noreply.github.com>
2024-10-19 16:37:39 +0530

    done

--- ITP_Project/backend/src/users/users.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('./users.controller');
-
-// Routes for user authentication
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-
-module.exports = router;
